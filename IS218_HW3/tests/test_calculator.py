451808c5c52544e8cc9c7d2b4e14c038ab4871a4
root <root@LAPTOP-7V76UN78>
2024-10-01 19:13:24 -0400

    adding test folder

---
+++ IS218_HW3/tests/test_calculator.py
@@ -0,0 +1,21 @@
+'''My Calculator Test'''
+from calculator import Calculator
+import pytest
+def test_addition():
+    '''Test that addition function works '''    
+    assert Calculator.add(2,2) == 4
+
+def test_subtraction():
+    '''Test that addition function works '''    
+    assert Calculator.subtract(2,2) == 0
+
+def test_multiply():
+    '''Test that Multiply function works'''
+    assert Calculator.multiply(2,2) == 4
+
+def test_divide():
+    '''Test that division function works'''
+    assert Calculator.divide(2,2) == 1
+def test_divide_by_zero():
+    with pytest.raises(ZeroDivisionError):
+        Calculator.divide(10,0)
