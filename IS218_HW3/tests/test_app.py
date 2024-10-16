7c4f4b7611e22942b563103535785bfa28d50a60
root <root@LAPTOP-7V76UN78>
2024-10-16 15:27:02 -0400

    adding test_app.py

--- IS218_HW3/tests/test_app.py
+++ IS218_HW3/tests/test_app.py
@@ -2,25 +2,23 @@ import pytest
 from app import App
 
 def test_app_start_exit_command(capfd, monkeypatch):
-    """Test that the REPL exits correctly on 'exit' command."""
-    # Simulate user entering 'exit'
     monkeypatch.setattr('builtins.input', lambda _: 'exit')
-    App.start()
-    out, err = capfd.readouterr()
+    app = App()
+    with pytest.raises(SystemExit) as e:
+        app.start()
+    assert e.type == SystemExit
 
-    # Check that the initial greeting is printed and the REPL exits gracefully
-    assert "Hello World. Type 'exit' to exit." in out
-    assert "Exiting..." in out
+
 
 def test_app_start_unknown_command(capfd, monkeypatch):
-    """Test how the REPL handles an unknown command before exiting."""
+    """Tests the REPL"""
     # Simulate user entering an unknown command followed by 'exit'
     inputs = iter(['unknown_command', 'exit'])
     monkeypatch.setattr('builtins.input', lambda _: next(inputs))
-    App.start()
-    out, err = capfd.readouterr()
-
-    # Check that the REPL responds to an unknown command and then exits after 'exit' command
-    assert "Hello World. Type 'exit' to exit." in out
-    assert "Unknown command. Type 'exit' to exit." in out
-    assert "Exiting..." in out
+    
+    app =App()
+    with pytest.raises(SystemExit):
+        app.start()  # Run the app    
+  
+    captured = capfd.readouterr()
+    assert "No such command: unknown_command" in captured.out
